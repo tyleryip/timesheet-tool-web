be22276733ef90d2378799531084eda7a6a687f1
Tyler Yip <tyler.yip1@ucalgary.ca>
2024-12-01 00:07:24 -0700

    feat: add doc comments to timesheet functions, add total time to output text box, add total time label to string constants

--- frontend/src/components/TextBoxes/OutputTextBox.tsx
+++ frontend/src/components/TextBoxes/OutputTextBox.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import TextBox from "./TextBox/TextBox";
 import { useAppSelector } from "../../store/hooks";
-import { selectOutput } from "../../store/slices/timesheetSlice";
-import { outputLabel, outputPlaceholder } from "../../constants/Strings";
+import {
+  getHoursString,
+  selectOutput,
+  selectTotalTime,
+} from "../../store/slices/timesheetSlice";
+import {
+  outputLabel,
+  outputPlaceholder,
+  totalTimeLabel,
+} from "../../constants/Strings";
 import CopyIconButton from "../Buttons/CopyIconButton";
 
 export default function OutputTextBox() {
   const output = useAppSelector(selectOutput);
+  const totalTime = useAppSelector(selectTotalTime) ?? 0;
+
+  const totalTimeLabelValue = totalTimeLabel.replace(
+    "{totalTime}",
+    getHoursString(totalTime)
+  );
+
+  // Only render total time label if it is non-zero
+  const showTotalTime = totalTime > 0 ? "" : "invisible";
 
   return (
     <div className="h-full w-full max-w-128 flex flex-col">
@@ -20,7 +37,17 @@ export default function OutputTextBox() {
           spellCheck={false}
           hideCaret={true}
         />
-        <CopyIconButton />
+        <div className="flex flex-row">
+          <div className="flex-none invisible">spacer</div>
+          <p
+            className={`${showTotalTime} grow text-center self-center text-sm md:text-base`}
+          >
+            {totalTimeLabelValue}
+          </p>
+          <div className="flex-none">
+            <CopyIconButton />
+          </div>
+        </div>
       </div>
     </div>
   );
