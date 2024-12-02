74c0a32f6cd8c609ef697b1a5cc54177ab5bb644
Tyler Yip <tyler.yip1@ucalgary.ca>
2024-12-01 20:47:10 -0700

    feat: refactor timesheet parsing functions and allow more flexible time formats, update unit tests

--- frontend/src/store/slices/timesheetSlice.test.ts
+++ frontend/src/store/slices/timesheetSlice.test.ts
@@ -1,4 +1,4 @@
-import { calculateTotalTime, parseTime, parseTimesheet, Task } from "./timesheetSlice";
+import { calculateDurationInHours, calculateTotalTime, parseTime, parseTimesheet, Task } from "./timesheetSlice";
 
 describe('testing timesheet slice functions', () => {
     // parseTime(timeStr: string): Date 
@@ -8,6 +8,20 @@ describe('testing timesheet slice functions', () => {
 
         var expected = new Date()
         expected.setHours(1, 0, 0, 0);
+
+        // Act
+        var actual = parseTime(timeString)
+
+        // Assert
+        expect(actual).toEqual(expected);
+    });
+
+    test('parseTime should return valid time with no minute value', () => {
+        // Arrange
+        var timeString = "2"
+
+        var expected = new Date()
+        expected.setHours(2, 0, 0, 0);
 
         // Act
         var actual = parseTime(timeString)
@@ -35,7 +49,27 @@ describe('testing timesheet slice functions', () => {
         var actual = parseTimesheet(rawTimesheet)
 
         // Assert
+        expect(actual).toEqual(expected);
+    });
 
+    test('parseTimesheet should parse one task with no minute values', () => {
+        // Arrange
+        var rawTimesheet =
+            `
+        Testing\n
+        9-10\n
+        `
+
+        var expected = Array<Task>();
+        expected.push({
+            name: "Testing",
+            hours: 1
+        })
+
+        // Act
+        var actual = parseTimesheet(rawTimesheet)
+
+        // Assert
         expect(actual).toEqual(expected);
     });
 
@@ -52,6 +86,32 @@ describe('testing timesheet slice functions', () => {
         expected.push({
             name: "Testing",
             hours: 1.5
+        })
+
+        // Act
+        var actual = parseTimesheet(rawTimesheet)
+
+        // Assert
+        expect(actual).toEqual(expected);
+    });
+
+    test('parseTimesheet should parse one task with multiple windows and mixed formats', () => {
+        // Arrange
+        var rawTimesheet =
+            `
+        Testing\n
+        9-10\n
+        10:30-11\n
+        11:00 - 12:00
+        12:30 - 1
+        1- 2
+        2:30 -3:00
+        `
+
+        var expected = Array<Task>();
+        expected.push({
+            name: "Testing",
+            hours: 4.5
         })
 
         // Act
@@ -110,7 +170,7 @@ describe('testing timesheet slice functions', () => {
     });
 
     // calculateTotalTime(tasks: Array<Task>): number
-    test('calculateTotalTime should correctly calculate total time', () => {
+    test('calculateTotalTime should calculate the correct total time', () => {
         // Arrange
         var timesheet = Array<Task>();
 
@@ -138,7 +198,7 @@ describe('testing timesheet slice functions', () => {
         expect(actual).toEqual(7.25)
     });
 
-    test('calculateTotalTime should correctly calculate total time when timesheet is empty', () => {
+    test('calculateTotalTime should calculate the correct total time when timesheet is empty', () => {
         // Arrange
         var timesheet = Array<Task>();
 
@@ -147,5 +207,62 @@ describe('testing timesheet slice functions', () => {
 
         //Assert
         expect(actual).toEqual(0)
+    });
+
+    // calculateDurationInHours(timespanStart: string, timespanEnd: string): number
+    test('calculateDurationInHours should calculate the correct duration', () => {
+        // Arrange
+        var start = "8:00"
+        var end = "9:30"
+
+        var expected = 1.5
+
+        // Act
+        var actual = calculateDurationInHours(start, end)
+
+        // Assert
+        expect(actual).toBe(expected)
+    });
+
+    test('calculateDurationInHours should calculate the correct duration when task starts at noon and ends after noon', () => {
+        // Arrange
+        var start = "12:00"
+        var end = "1:30"
+
+        var expected = 1.5
+
+        // Act
+        var actual = calculateDurationInHours(start, end)
+
+        // Assert
+        expect(actual).toBe(expected)
+    });
+
+    test('calculateDurationInHours should calculate the correct duration when task starts at noon and ends during noon', () => {
+        // Arrange
+        var start = "12:00"
+        var end = "12:45"
+
+        var expected = 0.75
+
+        // Act
+        var actual = calculateDurationInHours(start, end)
+
+        // Assert
+        expect(actual).toBe(expected)
+    });
+
+    test('calculateDurationInHours should calculate the correct duration when task in the morning and ends in the afternoon', () => {
+        // Arrange
+        var start = "11:00"
+        var end = "1:45"
+
+        var expected = 2.75
+
+        // Act
+        var actual = calculateDurationInHours(start, end)
+
+        // Assert
+        expect(actual).toBe(expected)
     });
 });
